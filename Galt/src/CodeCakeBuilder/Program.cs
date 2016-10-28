140a229811de5d909fb6c8cb924824ec6eb6e7e3
daubiht <daubihtmania@gmail.com>
2016-10-28 10:57:04 +0200

    Add CodeCakeBuilder, appveyor updated

---
+++ Galt/src/CodeCakeBuilder/Program.cs
@@ -0,0 +1,26 @@
+﻿using CodeCake;
+using System;
+using System.Linq;
+using System.IO;
+using Microsoft.Extensions.PlatformAbstractions;
+
+namespace CodeCakeBuilder
+{
+    public static class Program
+    {
+        public static int Main( string[] args )
+        {
+            string solutionDir = Path.GetDirectoryName( PlatformServices.Default.Application.ApplicationBasePath );
+            var app = new CodeCakeApplication( solutionDir, typeof(Program).Assembly );
+            bool interactive = !args.Contains( '-' + InteractiveAliases.NoInteractionArgument, StringComparer.OrdinalIgnoreCase );
+            int result = app.Run( args );
+            Console.WriteLine();
+            if( interactive )
+            {
+                Console.WriteLine( "Hit any key to exit. (Use -{0} parameter to exit immediately)", InteractiveAliases.NoInteractionArgument );
+                Console.ReadKey();
+            }
+            return result;
+        }
+    }
+}
---
+++ Galt/src/CodeCakeBuilder/build.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using Cake.Common.IO;
+using Cake.Common.Solution;
+using Cake.Common.Tools.MSBuild;
+using Cake.Common.Tools.NuGet;
+using Cake.Core;
+using Cake.Core.Diagnostics;
+
+namespace CodeCake
+{
+    /// <summary>
+    /// Sample build "script".
+    /// It can be decorated with AddPath attributes that inject paths into the PATH environment variable. 
+    /// </summary>
+    [AddPath( "CodeCakeBuilder/Tools" )]
+    public class Build : CodeCakeHost
+    {
+        public Build()
+        {
+
+            Task( "Clean" )
+                .Does( () =>
+                {
+                    Cake.CleanDirectories( "**/bin/", d => !d.Path.Segments.Contains( "CodeCakeBuilder" ) );
+                    Cake.CleanDirectories( "**/obj/", d => !d.Path.Segments.Contains( "CodeCakeBuilder" ) );
+                } );
+
+            Task( "Build" )
+                .IsDependentOn( "Clean" )
+                .Does( () =>
+                {
+                    using( var tempSln = Cake.CreateTemporarySolutionFile( "../../../../../../Galt.sln" ) )
+                    {
+                        tempSln.ExcludeProjectsFromBuild( "CodeCakeBuilder", "AnotherProjectIfNeeded" );
+                        Cake.MSBuild( tempSln.FullPath, new MSBuildSettings()
+                                .SetVerbosity( Verbosity.Minimal )
+                                .SetMaxCpuCount( 1 ) );
+                    }
+                } );
+
+            // The Default task for this script can be set here.
+            Task( "Default" )
+                .IsDependentOn( "Build" );
+        }
+    }
+}
