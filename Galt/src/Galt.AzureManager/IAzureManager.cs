3102164dc07cabc80a08b295bf857caf83798611
daubiht <daubihtmania@gmail.com>
2016-11-25 10:18:43 +0100

    Add azure tables interaction

---
+++ Galt/src/Galt.AzureManager/IAzureManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static Galt.AzureManager.AzureManager;
+
+namespace Galt.AzureManager
+{
+    public interface IAzureManager
+    {
+        void Initialize();
+
+        Task<bool> AddIfNotExists( string email, string pseudo );
+
+        Task<UserEntity> GetUser( string email, string pseudo );
+
+        Task<bool> AddGitHubTokenIfExists( string email, string pseudo, string token );
+
+        Task<bool> DeleteIfExists( string email, string pseudo );
+    }
+}
