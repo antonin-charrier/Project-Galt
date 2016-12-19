5bb315b83023e0acd004feec65a6fd8642b57690
Antonin CHARRIER <acharrier@intechinfo.fr>
2016-12-19 11:27:33 +0100

    Added VueResource to use HTTP requests

--- Galt/src/Galt/spa/src/main.js
+++ Galt/src/Galt/spa/src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VueResource from 'vue-resource'
 import VueRouter from 'vue-router'
 import App from './App.vue'
 import Home from './pages/Home.vue'
@@ -10,6 +11,7 @@ import AuthService from './services/AuthService'
 
 Vue.use(VueRouter);
 Vue.use(Vuex);
+Vue.use(VueResource);
 
 const routes = [
     { path: '/', component: Home },
