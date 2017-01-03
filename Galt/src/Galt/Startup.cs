67357273d846f629714a252658717f3558955e8f
Pixelmod <guillaumedanguin@yahoo.fr>
2017-01-03 10:58:38 +0100

    Made appsettings.json mandatory since app can't work without it

--- Galt/src/Galt/Startup.cs
+++ Galt/src/Galt/Startup.cs
@@ -17,7 +17,7 @@ namespace Galt
         {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
                 .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
                 .AddEnvironmentVariables();
 
