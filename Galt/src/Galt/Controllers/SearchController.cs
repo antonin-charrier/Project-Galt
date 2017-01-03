722cd5782739b43f1787f6826a68d2286fcfa18a
Pixelmod <guillaumedanguin@yahoo.fr>
2017-01-03 16:52:56 +0100

    Started the package searching on backend's side
    Need to continue this on a separate branch

---
+++ Galt/src/Galt/Controllers/SearchController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Galt.Services;
+using NuGet.Protocol.Core.Types;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Galt.Controllers
+{
+    [Route( "api /[ controller ]" )]
+    public class SearchController : Controller
+    {
+        readonly SearchService _searchService;
+
+        public SearchController(SearchService searchService)
+        {
+            _searchService = searchService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Search( [FromQuery] string searchTerm )
+        {
+            var results = await _searchService.Search( searchTerm );
+
+            return Ok( results );
+        }
+    }
+}
