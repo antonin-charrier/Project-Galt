99d45caed5e517bec23a1f077d0dc4e318347e70
Antonin CHARRIER <acharrier@intechinfo.fr>
2017-01-11 16:53:49 +0100

    Switching all Created stuff to Computed

--- Galt/src/Galt/spa/src/pages/Package.vue
+++ Galt/src/Galt/spa/src/pages/Package.vue
@@ -3,21 +3,20 @@
             <h1 class="package-name"><a class="package-link" :href="'https://www.nuget.org/packages/'+packageName" target="_blank">{{ packageName }}</a></h1>
             <div class="package-info">
             <div class="flex-bloc">
-                <h3 class="flex-info-text">
+                <h4 class="flex-info-text">
                     <div class="w3-dropdown-hover">
-                        <button class="w3-btn w3-white" v-on:click="displayVersions"><span class="actual-version">Version {{ packageCurrentVersion }}</span><div style="font-size: 30px; color: grey; padding-left: 10px" class="fa fa-sort-desc"></div></button>
-                        <div class="w3-dropdown-content w3-border version-options" v-if="versions">
-                            <version-option></version-option>
-
+                        <button class="w3-btn w3-white" v-on:click="displayVersions"><span class="actual-version">Version {{ packageVersion }}</span><div style="font-size: 20px; color: grey; padding-left: 10px" class="fa fa-sort-desc"></div></button>
+                        <div class="w3-dropdown-content w3-border version-options" v-if="versionsDisplayed">
+                            <versions-dropdown v-for="version in versions" :package="request"></versions-dropdown>
                         </div>
                     </div>
-                    <span class="flex-info-item">By</span><a class="flex-info-item-bis package-link" :href="'https://www.nuget.org/profiles/'+packageOwner" target="_blank">{{ packageOwner }}</a>
-                    <span class="flex-info-item">Published on</span><span class="flex-info-item-bis">{{ packagePDate }}</span>
-                </h3>
+                    <span class="flex-info-item">By {{ authors }}</span>
+                    <span class="flex-info-item">Published on {{ date }}</span>
+                </h4>
                 <i class="fa fa-star fa-star-orange" v-if="fav" v-on:click="addFav"></i>
                 <i class="fa fa-star fa-star-grey" v-if="!fav" v-on:click="addFav"></i>
             </div>
-            <p id="description">{{ packageDescription }}</div>
+            <p id="description">{{ description }}</div>
             <div class="flex-bloc">
                 <graph></graph>
                 <div class="flex-issues-versions">
@@ -37,22 +36,18 @@
 
 <script>
 import Graph from "../components/Graph.vue"
-
-var VersionsMenu = {
-  template: ''
-}
+import VersionsDropdown from "../components/VersionsDropdown.vue"
 
 export default {
     data: function () {
         return {
+            request: undefined,
+            ready: false,
             fav: false,
-            versions: false,
-            packageName : 'Code.Cake',  
-            packageCurrentVersion : '0.14.0',
-            packageVersions : ['0.14.0', '0.13.0', '0.12.0', '0.11.0', '0.10.0', '0.8.3', '0.8.2', '0.8.1', '0.8.0', '0.7.4', '0.7.3', '0.7.2', '0.7.1', '0.6.2', '0.6.1', '0.6.0', '0.6.0-r', '0.3.1', '0.3.0', '0.2.2', '0.2.1', '0.2.0', '0.1.0-r02', '0.1.0-beta'],
-            packageOwner : 'olivier-spinelli',
-            packagePDate : '10/19/2016',
-            packageDescription : 'Code.Cake library contains Code.Cake.dll (0.14.0) that CodeCakeBuilder applications uses.'
+            versionsDisplayed: false,
+            versions: [],
+            packageName : 'Cake.Core',
+            packageVersion : '0.14.0',
         }
     },
     methods: {
@@ -60,25 +55,40 @@ export default {
             this.fav = !this.fav
         },
         displayVersions: function () {
-            this.versions = !this.versions
+            this.versionsDisplayed = !this.versionsDisplayed
         }
     },
     created: function () {
-          this.$http.get('/api/package/infopackage?packageId=code.cake').then((response) => {
-            console.log(response);
-          }, (response) => {
-            console.log("test failed");
-          });
-    
-        VersionsMenu.template = VersionsMenu.template + '<div>';
-        for(var i=0; i<this.packageVersions.length ;i++){
-            VersionsMenu.template = VersionsMenu.template + '<router-link to="/package" href="#">Version ' + this.packageVersions[i] + '</router-link>'
+        this.$http.get('/api/package/infopackage?packageId=' + this.packageName).then((response) => {
+            this.request = JSON.parse(response.body);
+            this.versions = this.request.ListVPackage;
+            console.log(this.versions);
+            this.ready = true;
+            
+            for(var i=this.request.ListVPackage.length-1; i>=0; i--){
+                VersionsMenu.template = VersionsMenu.template + '<router-link to="/package" href="#">Version ' + this.request.ListVPackage[i] + '</router-link>'
+            }
+            VersionsMenu.template = VersionsMenu.template + '</div>';
+
+            console.log(this.request);
+        }, (response) => {
+            console.log("Request error");
+        });
+    },
+    computed: {
+        description: function() {
+            return this.ready ? this.request.Description : 'Loading...';
+        },
+        authors: function() {
+            return this.ready ? this.request.Authors.toString() : 'Loading';
+        },
+        date: function() {
+            return this.ready ? this.request.Timestamp : 'Loading';
         }
-        VersionsMenu.template = VersionsMenu.template + '</div>';
     },
     components: {
         'graph' : Graph,
-        'version-option' : VersionsMenu
+        'versions-dropdown' : VersionsDropdown
     }
 }
 </script>
@@ -106,9 +116,6 @@ export default {
     }
     .flex-info-item{
         margin-left: 40px;
-    }
-    .flex-info-item-bis{
-        margin-left: 10px;
     }
     .w3-dropdown-hover{
         margin-top: -5px;
